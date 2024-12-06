1eeacbc1465c4ead2e47b959b5ac0616d224faa6
Nyikon Zoltán Ferenc <nyikon.zoli@gmail.com>
2024-12-06 12:15:16 +0100

    apos fix 2

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ export const Hero = () => {
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              Let's turn our wate into opportunity!
+              Let&apos;s turn our wate into opportunity!
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
               We are EcoConnect. Our platform helps companies to sell or donate
