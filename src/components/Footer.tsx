f0a8164148ca7a7faab4b6c6ab42b4f3cee7c560
Nyikon Zoltán Ferenc <nyikon.zoli@gmail.com>
2024-12-06 12:29:06 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,120 +9,37 @@ export function Footer() {
   return (
     <div className="relative">
       <Container>
-        <div className="grid max-w-screen-xl grid-cols-1 gap-10 pt-10 mx-auto mt-5 border-t border-gray-100 dark:border-trueGray-700 lg:grid-cols-5">
-          <div className="lg:col-span-2">
-            <div>
-              {" "}
-              <Link
-                href="/"
-                className="flex items-center space-x-2 text-2xl font-medium text-primary dark:text-gray-100"
-              >
-                <Image
-                  src="/img/ecoconnect-logo.png"
-                  alt="N"
-                  width="128"
-                  height="128"
-                  className="w-28 rounded-lg mr-4"
-                />
-                <span className="font-bold text-xxl">ECOCONNECT</span>
-              </Link>
-            </div>
-
-            <div className="max-w-md mt-4 text-gray-500 dark:text-gray-400">
-            Our platform helps companies to sell or donate products they otherwise can’t — like items close to expiration, with damaged packaging, or minor defects — to individuals and non-profits.
-            </div>
-
-            <div className="mt-5">
-              <a
-                href="https://vercel.com/?utm_source=web3templates&utm_campaign=oss"
-                target="_blank"
-                rel="noopener"
-                className="relative block w-44"
-              >
-                <Image
-                  src="/img/vercel.svg"
-                  alt="Powered by Vercel"
-                  width="212"
-                  height="44"
-                />
-              </a>
-            </div>
-          </div>
-
-          <div>
-            <div className="flex flex-wrap w-full -mt-2 -ml-3 lg:ml-0">
-              {navigation.map((item, index) => (
-                <Link
-                  key={index}
-                  href="/"
-                  className="w-full px-4 py-2 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-trueGray-700"
-                >
-                  {item}
-                </Link>
-              ))}
-            </div>
-          </div>
-          <div>
-            <div className="flex flex-wrap w-full -mt-2 -ml-3 lg:ml-0">
-              {legal.map((item, index) => (
-                <Link
-                  key={index}
-                  href="/"
-                  className="w-full px-4 py-2 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-trueGray-700"
-                >
-                  {item}
-                </Link>
-              ))}
-            </div>
-          </div>
-          <div className="">
-            <div>Follow us</div>
-            <div className="flex mt-5 space-x-5 text-gray-400 dark:text-gray-500">
-              <a
-                href="https://twitter.com/web3templates"
-                target="_blank"
-                rel="noopener"
-              >
-                <span className="sr-only">Twitter</span>
-                <Twitter />
-              </a>
-              <a
-                href="https://facebook.com/web3templates"
-                target="_blank"
-                rel="noopener"
-              >
-                <span className="sr-only">Facebook</span>
-                <Facebook />
-              </a>
-              <a
-                href="https://instagram.com/web3templates"
-                target="_blank"
-                rel="noopener"
-              >
-                <span className="sr-only">Instagram</span>
-                <Instagram />
-              </a>
-              <a href="https://linkedin.com/" target="_blank" rel="noopener">
-                <span className="sr-only">Linkedin</span>
-                <Linkedin />
-              </a>
-            </div>
+        <div className="w-full pt-10 mx-auto mt-5 border-t border-gray-100 dark:border-trueGray-700 lg:grid-cols-5">
+          <div className="w-full">
+            <Link
+              href="/"
+              className="flex flex-row items-center space-x-2 text-2xl font-medium text-primary justify-center w-full"
+            >
+              <Image
+                src="/img/ecoconnect-logo.png"
+                alt="N"
+                width="128"
+                height="128"
+                className="w-12 rounded-lg mr-2"
+              />
+              <span className="font-bold text-xxl text-center">ECOCONNECT</span>
+            </Link>
           </div>
         </div>
-
+        <p className=" pt-8 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl dark:text-gray-300 text-center">
+          info@ecoconnect.hu
+        </p>
+        <p className=" pt-4 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl dark:text-gray-300 text-center">
+          +36 30 844 6803
+        </p>
         <div className="my-10 text-sm text-center text-gray-600 dark:text-gray-400">
-          Copyright © {new Date().getFullYear()}. Made with ♥ by{" "}
-          <a href="https://web3templates.com/" target="_blank" rel="noopener">
-            Web3Templates.
-          </a>{" "}
+          Copyright © {new Date().getFullYear()}. Made with ♥ by EcoConnect.
           Illustrations from{" "}
-          <a href="https://www.glazestock.com/" target="_blank" rel="noopener ">
-            Glazestock
+          <a href="https://storyset.com/" target="_blank" rel="noopener ">
+            Storyset
           </a>
         </div>
       </Container>
-      {/* Do not remove this */}
-      <Backlink />
     </div>
   );
 }
